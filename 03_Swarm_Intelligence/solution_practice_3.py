060984a8e469ac315ed93ec3de65aa9eae010e84
JesusCarramza <eduardo.carranza.mercado@gmail.com>
2026-04-01 21:32:25 -0600

    feat: initialize program

--- 03_Swarm_Intelligence/solution_practice_3.py
+++ 03_Swarm_Intelligence/solution_practice_3.py
@@ -12,3 +12,19 @@ def funcion_objetivo(x, y):
     # f(x,y) = x^2 + y^2 + [25 * (senx + seny)]
     return x**2 + y**2 + 25 * (np.sin(x) + np.sin(y))
 
+# --- 3. Inicialización aleatoria de posiciones y VELOCIDAD CERO ---
+# Las posiciones deben estar en el intervalo (-5, 5)
+posiciones = np.random.uniform(-5, 5, (N, 2))
+
+# Velocidad inicial en 0 para todas las partículas
+velocidades = np.zeros((N, 2))
+
+# Inicialización de pbest (mejor posición propia)
+pbest_pos = np.copy(posiciones)
+pbest_val = np.array([funcion_objetivo(p[0], p[1]) for p in posiciones])
+
+# Inicialización de gbest (mejor posición global para minimización)
+gbest_idx = np.argmin(pbest_val)
+gbest_pos = np.copy(pbest_pos[gbest_idx])
+gbest_val = pbest_val[gbest_idx]
+

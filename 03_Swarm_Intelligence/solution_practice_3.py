a106935fcaa4e54082711bb4c1f54bc3077261d1
JesusCarramza <eduardo.carranza.mercado@gmail.com>
2026-04-01 21:15:53 -0600

    feat: define execution parameters

---
+++ 03_Swarm_Intelligence/solution_practice_3.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+# --- 1. Definir parámetros de ejecución ---
+N = 20           # Número de partículas
+iteraciones = 50 # Número de iteraciones
+a = 0.4          # Inercia
+b1 = 0.7         # Aprendizaje local (Influencia propia)
+b2 = 1.2         # Aprendizaje global (Influencia social)

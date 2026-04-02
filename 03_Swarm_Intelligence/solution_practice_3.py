8e97e8209eb2efe7fcc0843068fe5b7bad6f5471
JesusCarramza <eduardo.carranza.mercado@gmail.com>
2026-04-01 21:16:38 -0600

    feat: define objective function

--- 03_Swarm_Intelligence/solution_practice_3.py
+++ 03_Swarm_Intelligence/solution_practice_3.py
@@ -6,3 +6,9 @@ iteraciones = 50 # Número de iteraciones
 a = 0.4          # Inercia
 b1 = 0.7         # Aprendizaje local (Influencia propia)
 b2 = 1.2         # Aprendizaje global (Influencia social)
+
+# --- 2. Definir función objetivo ---
+def funcion_objetivo(x, y):
+    # f(x,y) = x^2 + y^2 + [25 * (senx + seny)]
+    return x**2 + y**2 + 25 * (np.sin(x) + np.sin(y))
+
